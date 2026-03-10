7c4eb3de7d13178afe91a681cb950e2bc9007f11
dimas pratama <12345678+dimaspratama9vq@gmail.com>
2026-03-10 14:11:35 +0700

    fix database url

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+
+database_url = os.environ.get("DATABASE_URL", "sqlite:///" + os.path.join(basedir, "review.db"))
+
+# Fix untuk Render (postgres:// → postgresql://)
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
